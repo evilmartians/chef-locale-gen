92ac6b79c8fca33c8f0c00b4346381de45204eac
Kirill Kouznetsov <agon.smith@gmail.com>
2019-12-23 17:09:40 +0300

    version 0.0.7 which supports chef 15

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'kir@evilmartians.com'
 license          'Apache-2.0'
 description      'Adds new locales and generates them'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.6'
+version          '0.0.7'
 
-chef_version '>= 12.0', '< 15.0'
+chef_version '>= 12.0', '< 16.0'
 
 supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 7.0'
